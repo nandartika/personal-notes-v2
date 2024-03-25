073991c9364b732a96b1abfb6cb104536ef6a277
nandartika <kartikanandap@gmail.com>
2024-03-25 09:47:07 +0700

    add: routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react';
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
 
 function App() {
-  return (
-    <div className="app-container">
-      <h1>Hello, React</h1>
-    </div>
-  );
+  return <Routes>
+    <Route path='/' element={<h1>Container</h1>}>
+      <Route path='login' element={<h1>Login Page</h1>} />
+      <Route path='sign-up' element={<h1>Sign Up Page</h1>} />
+      <Route index path='/' element={<h1>Catatan Active</h1>} />
+      <Route path='/archives' element={<h1>Catatan Archive</h1>} />
+    </Route>
+  </Routes>
 }
 
-export default App;
+export default App
