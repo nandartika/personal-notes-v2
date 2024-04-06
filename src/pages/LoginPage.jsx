acfc8384c05b26c083f59838394b719ee0aa7f37
nandartika <kartikanandap@gmail.com>
2024-04-06 21:52:32 +0700

    add: function login

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,22 +1,50 @@
-import React from 'react'
-import Input from '../components/Input'
+import React from "react";
+import Input from "../components/Input";
+import useInputChange from "../hooks/useInputChange";
+import { login, putAccessToken } from "../utils/network-data";
+import { useNavigate } from "react-router-dom";
 
 function LoginPage() {
+  const [email, handleEmailChange] = useInputChange("");
+  const [password, handlePasswordChange] = useInputChange("");
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const { data } = await login({ email, password });
+    if (data?.accessToken) {
+      putAccessToken(data?.accessToken);
+      navigate("/notes");
+    }
+  };
+
   return (
-    <section className='login-page'>
+    <section className="login-page">
       <h2>Yuk, login untuk menggunakan aplikasi.</h2>
 
-      <div className='input-login'>
-        <Input id='email' type='email' label='Email' />
-        <Input id='password' type='password' label='Password' />
-        <button type='button'>Login</button>
-      </div>
+      <form className="input-login" onSubmit={onSubmitHandler}>
+        <Input
+          id="email"
+          type="email"
+          label="Email"
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <Input
+          id="password"
+          type="password"
+          label="Password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button type="submit">Login</button>
+      </form>
 
       <p>
-        Belum punya akun? <a href='/register'>Daftar di sini</a>
+        Belum punya akun? <a href="/register">Daftar di sini</a>
       </p>
     </section>
-  )
+  );
 }
 
-export default LoginPage
+export default LoginPage;
