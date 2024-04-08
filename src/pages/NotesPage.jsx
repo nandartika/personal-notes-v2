6f4619d5cb4d2d19767be5028fb6dcb308a91402
nandartika <kartikanandap@gmail.com>
2024-04-09 06:48:43 +0700

    add: search function

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -7,10 +7,18 @@ import NotesList from "../components/NotesList";
 import Input from "../components/Input";
 import FloatButton from "../components/FloatButton";
 import { FiPlus as PlusIcon } from "react-icons/fi";
+import useInputChange from "../hooks/useInputChange";
 
 function NotesPage({ type }) {
   const navigate = useNavigate();
   const [notes, setNotes] = useState();
+  const [keyword, onChangeKeywordInput] = useInputChange("");
+
+  const filteredNotes = keyword
+    ? notes.filter((note) =>
+        note.title.toLowerCase().includes(keyword.toLowerCase())
+      )
+    : notes;
 
   const isArchived = type === "archived";
 
@@ -34,10 +42,14 @@ function NotesPage({ type }) {
       <h2>Catatan{isArchived ? "Arsip" : "Aktif"}</h2>
 
       <section className="search-bar">
-        <Input placeholder="Cari berdasarkan judul ..." />
+        <Input
+          placeholder="Cari berdasarkan judul ..."
+          value={keyword}
+          onChange={onChangeKeywordInput}
+        />
       </section>
 
-      {notes && <NotesList notes={notes} />}
+      {filteredNotes && <NotesList notes={filteredNotes} />}
 
       <section className="homepage__action">
         <FloatButton title="Tambah" onClick={handleAddButtonClick}>
